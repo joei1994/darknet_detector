79259594583cf77ca8b1d58dd642b88190f9e56c
Joei <wachirawit.iedchoo@gmail.com>
2019-08-14 17:34:26 +0700

    add vid_streamer.py

---
+++ vid_streamer.py
@@ -0,0 +1,75 @@
+import cv2
+import os
+import time
+
+from cardetection.detector import Detector
+from cardetection.captor import Captor
+from cardetection.tracker.sort import Sort
+
+current_milli_time = lambda: int(round(time.time() * 10000))
+
+def draw_bbox(frame, detections):
+    for det in detections:
+        xmin, ymin, xmax, ymax = det['xmin'], det['ymin'], det['xmax'], det['ymax']
+        
+        pt1 = (xmin, ymin)
+        pt2 = (xmax, ymax)
+
+        cv2.rectangle(frame, pt1, pt2, (0, 255, 0), 1)
+        
+        cv2.putText(frame, f"{det['label']}", (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, [0, 255, 0], 2)
+    return frame
+
+def create_roi_box(x, y, width, height):
+    roi_box = {
+        'xmin': int(x - width / 2),
+        'ymin': int(y - height / 2),
+        'xmax': int(x + width / 2),
+        'ymax': int(y + height / 2)
+    }
+
+    return roi_box
+
+def save_frame(frame, track, output_dir='./capture'):
+    margin = 30
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    cropped_frame = frame[track[1] - margin:track[3] + margin, track[0] - margin:track[2] + margin]
+    ouptut_path = os.path.join(output_dir, f'{current_milli_time()}.jpg')
+    if not os.path.exists(ouptut_path):
+        cv2.imwrite(ouptut_path, cropped_frame)
+    
+    
+    
+def main():
+    frameWidth, frameHeight = 1920, 1080  
+    
+    detector = Detector()
+    #roi_box = create_roi_box(x=680, y=570, width=640, height=400)  
+    roi_box = create_roi_box(x=680, y=520, width=560, height=310)
+    captor = Captor(Sort(), ['car', 'truck'], roi_box)
+
+    #cap = cv2.VideoCapture('rtsp://admin:iapp2019@192.168.1.64/1')
+    cap = cv2.VideoCapture('C:/Users/iApp/Web/RecordFiles/2019-07-24/output.mp4')
+    cap.set(3, frameWidth)
+    cap.set(4, frameHeight)
+
+    while True:
+        _, frame = cap.read()
+        
+        detections = detector.detect(frame)
+        capturedDetection = captor.capture(detections)
+        if capturedDetection is not None:
+            save_frame(frame, capturedDetection)
+    
+        frame = draw_bbox(frame, detections)
+        cv2.rectangle(frame, (roi_box['xmin'], roi_box['ymin']), (roi_box['xmax'], roi_box['ymax']), (255, 0, 0), 1)
+        cv2.imshow('Demo', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+
+
+if __name__ == '__main__':
+    main()
